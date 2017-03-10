35e058244db9465bf43acb78ffee9e5d5b0527c2
anandtadela <anand2313@gmail.com>
2017-03-10 12:34:36 +0530

    validation changes

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,15 @@
 "use strict";
 angular.module('myApp.controllers', []).controller('View1Controller', function($scope, $http, $location,myService) {
+    $scope.showErrorMsg = false;
+    $scope.onClientNameChange =  function(value) {
+        $scope.showErrorMsg = !!value ? false:true;
+    }
     $scope.getClientDetails = function() {
-                $http({
+        if(!!$scope.clientName) {
+             $http({
                 method: 'GET',
                 url: 'json/viewdata.json',
-                params: {client_number: $scope.clientNumber}
+                params: {client_name: $scope.clientName}
             }).then(function (data){
                 var resultData = data.data;
                 myService.set(resultData);
@@ -12,7 +17,15 @@ angular.module('myApp.controllers', []).controller('View1Controller', function($
             },function (error){
                 console.log("error");
             });
+        } else {
+            $scope.showErrorMsg = true;
+            return;
+        }
+               
     }
 }).controller('View2Controller', function($scope, $http, $location,myService) {
     $scope.data1 = myService.get();
+    $scope.backToHome = function() {
+        $location.path("/view1");
+    }
 });
