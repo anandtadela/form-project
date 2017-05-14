8c14eddd42c43c3d2e1d77f23bfe2f7e57c7449f
Sri Hari Rao Dixit -X (srdixit - LARSEN Srdixit TOUBRO INFOTECH LTD at Cisco) <srdixit@cisco.com>
2017-05-14 11:01:02 -0400

    NextGen angular header updates

--- js/app.js
+++ js/app.js
@@ -4,17 +4,15 @@ angular.module('myApp',[
 'myApp.controllers',
 'ui.bootstrap'
 ]).config(function($routeProvider, $locationProvider){
-    $routeProvider.when('/view1',{
-    templateUrl:'/partials/view1.html'
-    }).when('/default',{
-        templateUrl:'/partials/default.html'
+    $routeProvider.when('/home',{
+    templateUrl:'/partials/home.html'
     }).when('/view2',{
     templateUrl:'/partials/view2.html',
     reloadOnSearch: false 
     }).when('/submitform',{
     templateUrl:'/partials/submitform.html',
     reloadOnSearch: false 
-    }).otherwise({redirectTo:'/default'});
+    }).otherwise({redirectTo:'/home'});
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
